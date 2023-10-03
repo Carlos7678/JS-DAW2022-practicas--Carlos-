a2b250136f3cfbd73ee37a40b060e1249142c028
Carloscontacto4d@gmail.com <carloscontacto4d@gmail.com>
2023-10-03 08:14:08 +0200

    ej17

---
+++ 2DAW/DEC/Ejer17-UD1. Map y Set #1/mapSet.js
@@ -0,0 +1,8 @@
+let modulos = new Map();
+modulos.set('DAW', 'Despliege de aplicaciones web');
+modulos.set('DEC', 'Desarrollo Web en Entorno Cliente');  
+modulos.set('DWS', 'Desarrollo Web en Entorno Servidor');
+modulos.set('DIW', 'Diseño de interfaces web');   // un string como clave
+
+console.log(modulos.has('DAW'));
+console.log(modulos.has('GBD'));
