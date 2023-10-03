20d2a0f1e220cd4e0ed0df26fca4ec73f402b93f
Carloscontacto4d@gmail.com <carloscontacto4d@gmail.com>
2023-10-03 08:45:46 +0200

    ej 17 revisado

--- 2DAW/DEC/Ejer17-UD1. Map y Set #1/mapSet.js
+++ 2DAW/DEC/Ejer17-UD1. Map y Set #1/mapSet.js
@@ -4,5 +4,34 @@ modulos.set('DEC', 'Desarrollo Web en Entorno Cliente');
 modulos.set('DWS', 'Desarrollo Web en Entorno Servidor');
 modulos.set('DIW', 'Diseño de interfaces web');   // un string como clave
 
+console.log('La cantidad total de modulos son: '+modulos.size)
+
+iterator1 = modulos.keys();
+valor = modulos.values();
+
+for (const element of modulos) {
+    console.log('La abreviatura es '+iterator1.next().value+ " y su nombre completo es "+valor.next().value );
+  }
+
+iterator1 = modulos.keys();
+console.log('Abreviaturas')
+  for (const element of modulos) {
+   console.log(iterator1.next().value)
+  }
+
+
+valor = modulos.values();
+console.log('Los nombres completos de los modulos son: ')
+  for (const element of modulos) {
+      console.log(valor.next().value );
+    }
+
 console.log(modulos.has('DAW'));
 console.log(modulos.has('GBD'));
+
+modulos.delete('DIW')
+iterator1 = modulos.keys();
+valor = modulos.values();
+for (const element of modulos) {
+    console.log('La clave -> '+iterator1.next().value+ " y su valor -> "+valor.next().value );
+  }
