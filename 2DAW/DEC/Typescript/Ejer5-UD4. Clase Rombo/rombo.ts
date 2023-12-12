485f9052306d39e90800532e403e4858469e5cb6
Carloscontacto4d@gmail.com <carloscontacto4d@gmail.com>
2023-12-12 08:44:42 +0100

    ejercicios ts 12/12

---
+++ 2DAW/DEC/Typescript/Ejer5-UD4. Clase Rombo/rombo.ts
@@ -0,0 +1,15 @@
+class Rombo {
+    #longitudDiagonalH;
+    #longitudDiagonalV;
+
+    constructor(longitudDiagonalH, longitudDiagonalV) {
+        this.#longitudDiagonalH = longitudDiagonalH;
+        this.#longitudDiagonalV = longitudDiagonalV;
+    }
+
+    calcularArea = () => this.#longitudDiagonalH * this.#longitudDiagonalV;
+}
+
+const rombo = new Rombo(10, 15);
+const area = rombo.calcularArea();
+console.log("Área:", area);
